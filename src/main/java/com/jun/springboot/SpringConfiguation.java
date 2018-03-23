00336f7d669117696b414a4ff298c4135a61b7a4
luoa <luoa@GZJF020413.gd.cnc.intra>
2018-03-23 14:42:41 +0800

    增加  spring config文件

---
+++ src/main/java/com/jun/springboot/SpringConfiguation.java
@@ -0,0 +1,26 @@
+package com.jun.springboot;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+/**
+ * 配置  Spring config的地方
+ * @author jun
+ * @date 2018年3月23日 : 上午10:08:50
+ */
+@Configuration
+@EnableAutoConfiguration
+public class SpringConfiguation {
+		
+	/**
+	 * 注入   ThreadPoolTaskExecutor
+	 * @return
+	 */
+	@Bean
+	@ConfigurationProperties(prefix="spring.taskExecutor")
+	public ThreadPoolTaskExecutor initialThreadPoolTaskExecutor(){
+		return new ThreadPoolTaskExecutor();
+	}
+}
